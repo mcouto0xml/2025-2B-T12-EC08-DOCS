de2c0d3eb8b61ebd3dfd2f4923c32e6f958ec4b4
GustavoWidman (laptop-mac) <admin@r3dlust.com>
2025-10-20 17:29:05 -0300

    feat(workflows): swap to a "preview" deployment

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -6,6 +6,8 @@ const withMDX = createMDX();
 const config = {
 	reactStrictMode: true,
 	output: "export",
+	basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+	assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
 };
 
 export default withMDX(config);
